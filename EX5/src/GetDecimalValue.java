6f71ed0f061f1d5cb0377018258d3c0bc98ee80b
satya-sh <satyanshbu@gmail.com>
2024-03-04 11:26:42 -0800

    EX5 completed.

---
+++ EX5/src/GetDecimalValue.java
@@ -0,0 +1,10 @@
+public class GetDecimalValue {
+    public int getDecimalValue(ListNode head) {
+        int num = head.val;
+        while (head.next != null) {
+            num = num * 2 + head.next.val;
+            head = head.next;
+        }
+        return num;
+    }
+}
