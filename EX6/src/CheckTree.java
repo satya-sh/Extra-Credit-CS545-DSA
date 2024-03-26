bbec8e5eca402fc9da9a9491873f1acad156d50b
satya-sh <satyanshbu@gmail.com>
2024-03-25 22:10:42 -0700

    EX6 completed.

---
+++ EX6/src/CheckTree.java
@@ -0,0 +1,5 @@
+public class CheckTree {
+    public boolean checkTree(TreeNode root) {
+        return root.val == (root.left.val + root.right.val);
+    }
+}
---
+++ EX6/src/SearchBST.java
@@ -0,0 +1,15 @@
+public class SearchBST {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        }
+
+        else {
+            return searchBST(root.right, val);
+        }
+    }
+}
